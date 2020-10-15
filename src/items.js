3f2b7b5df1a1e4d2101a3840e90d25b92bad3e71
osha7 <ogroetz@gmail.com>
2020-10-14 19:25:55 -0600

    css allows images to fit

--- src/items.js
+++ src/items.js
@@ -31,9 +31,11 @@ class Items extends React.Component {
                             {itemsArray.map(item => (
                                 // console.log(item),
                                 <div key={item.id} className={`flexbox-item flexbox-item-${item.id}`}>
-                                    {/* <div className="item-image"><img src={item.image} /></div> */}
+                                    <div className="item-image"><img src={item.image} /></div>
 
                                     <div className='h5'><h5>{item.title}</h5></div>
+                                    {/* https://stackoverflow.com/questions/1726630/formatting-a-number-with-exactly-two-decimals-in-javascript */}
+                                    <div>${item.price.toFixed(2)}</div>
                                 </div>
                             ))}
                     </div>
